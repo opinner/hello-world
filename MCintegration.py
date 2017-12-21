8689d0afd8467cdc74eb7a04d4e8193c180b1947
opinner <32843689+opinner@users.noreply.github.com>
2017-12-21 14:01:21 +0100

    ising integration

--- MCintegration.py
+++ MCintegration.py
@@ -2,11 +2,12 @@ import numpy as np
 import math as m
 import matplotlib.pyplot as plt
 
-def f(x):
+
+def h(x):
     return (np.cos(50*x)+np.sin(20*x))**2
 
 
-def h(x):
+def f(x):
     return 1
 
 
@@ -17,13 +18,13 @@ def mc_integration(a,b,N,f,h):
     x_values = np.random.uniform(a,b,N)
 
     for i in range(N):
-        summe = summe + f(x_values[i])
+        summe = summe + h(x_values[i])
         progress.append(summe/(i+1))
-        #print(i,summe/(i+1))
+        print(i,summe/(i+1))
 
     return progress
 
-N = 1e6
+N = 1e7
 
 progress = np.asarray(mc_integration(0,1,N,f,h))
 
@@ -31,5 +32,5 @@ steps = np.arange(1,N+1)
 
 print(progress[-1])
 plt.plot(np.log(steps),progress)
-plt.axis([0,np.log(N),0.7,1.05])
+#plt.axis([0,np.log(N),0.7,1.05])
 plt.show() 
